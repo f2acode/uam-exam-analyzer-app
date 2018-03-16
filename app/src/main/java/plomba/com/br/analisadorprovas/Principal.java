48a4bd7e0e9ce14a57f080be9036b1c8c93fb369
felipeaugustox <augustto.felipe@hotmail.com>
2018-03-16 01:43:51 -0300

    pegando foto e mostrando tumbnail

--- app/src/main/java/plomba/com/br/analisadorprovas/Principal.java
+++ app/src/main/java/plomba/com/br/analisadorprovas/Principal.java
@@ -1,7 +1,12 @@
 package plomba.com.br.analisadorprovas;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Principal extends AppCompatActivity {
 
@@ -10,4 +15,30 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void tirarFoto(View view){
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        ImageView imageView = (ImageView) findViewById(R.id.img_view);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
