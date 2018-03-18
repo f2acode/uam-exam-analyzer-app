5f4d48e879dabbf1212b1238f20d6489b796ac64
felipeaugustox <augustto.felipe@hotmail.com>
2018-03-18 15:48:01 -0300

    tirando

--- app/src/main/java/plomba/com/br/analisadorprovas/Principal.java
+++ app/src/main/java/plomba/com/br/analisadorprovas/Principal.java
@@ -2,11 +2,21 @@ package plomba.com.br.analisadorprovas;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Principal extends AppCompatActivity {
 
@@ -16,7 +26,8 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    Uri photoURI;
 
     public void tirarFoto(View view){
         dispatchTakePictureIntent();
@@ -24,8 +35,26 @@ public class Principal extends AppCompatActivity {
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // ensures that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // creates the File where the photo should go
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch(IOException ex){
+                // error occurred while creating the file
+                ex.printStackTrace();
+            }
+            // continues only if the file was successfully created
+            if(photoFile != null){
+                // Here we don't receive data on onActivityResult - because of the EXTRA_OUTPUT
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+
+                photoURI = Uri.fromFile(photoFile);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
@@ -34,11 +63,31 @@ public class Principal extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.img_view);
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+            Uri uri = photoURI;
+
+            Bitmap imageBitmap = BitmapFactory.decodeFile(uri.getPath());
             imageView.setImageBitmap(imageBitmap);
         }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException{
+        // create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        );
+
+        // save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
