963ec316102bda59df85bf00d8fce9983583db5c
felipeaugustox <augustto.felipe@hotmail.com>
2018-03-28 00:34:28 -0300

    tela principal

--- app/src/main/java/plomba/com/br/analisadorprovas/Principal.java
+++ app/src/main/java/plomba/com/br/analisadorprovas/Principal.java
@@ -1,17 +1,12 @@
 package plomba.com.br.analisadorprovas;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,20 +15,16 @@ import java.util.Date;
 
 public class Principal extends AppCompatActivity {
 
+    static final int REQUEST_TAKE_PHOTO = 1;
+    Uri photoURI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
     }
 
-    static final int REQUEST_TAKE_PHOTO = 1;
-    Uri photoURI;
-
-    public void tirarFoto(View view){
-        dispararIntentTirarFoto();
-    }
-
-    private void dispararIntentTirarFoto() {
+    public void dispararIntentTirarFoto(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         // ensures that there's a camera activity to handle the intent
@@ -41,7 +32,7 @@ public class Principal extends AppCompatActivity {
             // creates the File where the photo should go
             File photoFile = null;
             try{
-                photoFile = createImageFile();
+                photoFile = criarArquivoImagem();
             }catch(IOException ex){
                 // error occurred while creating the file
                 ex.printStackTrace();
@@ -53,7 +44,7 @@ public class Principal extends AppCompatActivity {
                         Uri.fromFile(photoFile));
 
                 photoURI = Uri.fromFile(photoFile);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
@@ -61,20 +52,20 @@ public class Principal extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        ImageView imageView = (ImageView) findViewById(R.id.img_view);
-
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
 
-            Uri uri = photoURI;
+            //ImageView imageView = (ImageView) findViewById(R.id.img_view);
 
-            Bitmap imageBitmap = BitmapFactory.decodeFile(uri.getPath());
-            imageView.setImageBitmap(imageBitmap);
+            //Uri uri = photoURI;
+            //Bitmap imageBitmap = BitmapFactory.decodeFile(uri.getPath());
+            //imageView.setImageBitmap(imageBitmap);
         }
     }
 
-    String mCurrentPhotoPath;
+    private File criarArquivoImagem() throws IOException{
 
-    private File createImageFile() throws IOException{
+        String mCurrentPhotoPath;
+
         // create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
